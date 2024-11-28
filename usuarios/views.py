2263b40b48c6956179cc734f7a7f19248d8d2d7b
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2024-11-28 05:38:37 -0300

    criando views de login e logout

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def login_view(request):
+    return HttpResponse('Tela de login')
+
+def logout_view(request):
+    return HttpResponse('Tela de logout')
