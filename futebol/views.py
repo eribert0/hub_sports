95438ff3e3bfc392caf8c9d41c4da596512d98fc
eribert0 <ierigeek@gmail.com>
2024-11-28 13:09:06 -0300

    add importação

--- futebol/views.py
+++ futebol/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from requests import request
 
 def futebol_home(request):
     return HttpResponse('Página Home - Futebol')
