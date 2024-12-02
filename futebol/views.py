0d23f72b73f6dd3da06309227ce948d7ba0c23e0
eribert0 <ierigeek@gmail.com>
2024-12-02 12:59:30 -0300

    alterando template

--- futebol/views.py
+++ futebol/views.py
@@ -26,16 +26,23 @@ def futebol_campeonatos(request):
     return render(request, 'campeonatos.html', {'campeonatos':campeonatos})
 
 def campeonato_detalhes(request, campeonato_id):
-    url = f'https://api.api-futebol.com.br/v1/campeonatos/{campeonato_id}/tabela'
+    url_tabela = f'https://api.api-futebol.com.br/v1/campeonatos/{campeonato_id}/tabela'
+    url_artilharia = f'https://api.api-futebol.com.br/v1/campeonatos/{campeonato_id}/artilharia'
     headers = {
         'Authorization':'Bearer live_b03f8374826632e4e1a376d7320f52'
     }
 
-    response = requests.get(url, headers=headers)
+    response_tabela = requests.get(url_tabela, headers=headers)
+    response_artilharia = requests.get(url_artilharia, headers=headers)
 
-    if response.status_code == 200:
-        tabela = response.json()
+    if response_tabela.status_code == 200 and response_artilharia.status_code == 200:
+        tabela = response_tabela.json()
+        artilharia = response_tabela.json()
     else:
-        tabela = {}
+        tabela = []
+        artilharia = []
 
-    return render(request, 'campeonato_detalhes.html', {'tabela':tabela})
+    return render(request, 'campeonato_detalhes.html', {
+        'tabela':tabela,
+        'artilheiros': artilharia
+    })
